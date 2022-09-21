f3f2dd202f65e7a6eee712517f47a2c4d1f887c8
Vladuslav Dragushin <maxhc@inbox.ru>
2022-09-22 00:08:01 +0300

    download spacex images

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import os
+import json
 
 import requests
 
 
-URL = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+
+
+URL = 'https://api.spacexdata.com/v5/launches/5eb87d42ffd86e000604b384'
 
 
 def download_image(url, folder):
     response = requests.get(url)
     response.raise_for_status()
-
     with open(folder, 'wb') as file:
         file.write(response.content)
 
@@ -21,9 +23,16 @@ def main():
 
     os.makedirs(books_folder, exist_ok=True)
 
-    filename = f'{books_folder}{os.sep}hubble.jpeg'
+    response = requests.get(URL)
+    response.raise_for_status()
+    links = response.json()['links']['flickr']['original']
+    image_count = 1
+    for link in links:
 
-    download_image(URL, filename)
+        filename = f'{books_folder}{os.sep}spacex_{image_count}.jpeg'
+        download_image(link, filename)
+
+        image_count += 1
 
 
 if __name__ == '__main__':
