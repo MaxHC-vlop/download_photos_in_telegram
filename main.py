3c4e9bc9c6c8956e0ef6f804620d814681075acf
Vladuslav Dragushin <maxhc@inbox.ru>
2022-09-21 23:14:42 +0300

    added download func

--- main.py
+++ main.py
@@ -5,22 +5,25 @@ import requests
 
 URL = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 
+
+def download_image(url, folder):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(folder, 'wb') as file:
+        file.write(response.content)
+
+
 def main():
     books_folder = os.path.join(
         'images',
         )
 
-    filename = f'{books_folder}{os.sep}hubble.jpeg'
-
-    print(books_folder)
-
     os.makedirs(books_folder, exist_ok=True)
 
-    response = requests.get(URL)
-    response.raise_for_status()
+    filename = f'{books_folder}{os.sep}hubble.jpeg'
 
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+    download_image(URL, filename)
 
 
 if __name__ == '__main__':
