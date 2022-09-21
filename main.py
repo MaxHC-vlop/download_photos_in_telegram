00f5fbd747a27b6e611d39ae39737ee429bde030
Vladuslav Dragushin <maxhc@inbox.ru>
2022-09-21 23:12:39 +0300

    make image directory

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+import os
+
 import requests
 
 
 URL = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-FILENAME = 'hubble.jpeg'
 
 def main():
+    books_folder = os.path.join(
+        'images',
+        )
+
+    filename = f'{books_folder}{os.sep}hubble.jpeg'
+
+    print(books_folder)
+
+    os.makedirs(books_folder, exist_ok=True)
+
     response = requests.get(URL)
     response.raise_for_status()
 
-    with open(FILENAME, 'wb') as file:
+    with open(filename, 'wb') as file:
         file.write(response.content)
 
 
